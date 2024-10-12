740d4f9eac35de037b636a03c33d27cf8b5a52bc
Peter Repukat <orly.alia5@gmail.com>
2024-10-12 04:18:50 +0200

    chore: Add FreeRTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,61 @@
+#include <FreeRTOS.h>
+#include <task.h>
+#include <queue.h>
+#include <timers.h>
+#include <semphr.h>
+
 #include <stdio.h>
 #include "pico/stdlib.h"
 
 
-int main() {
-    stdio_init_all();
+static void exampleTask(void* parameters) {
+    (void)parameters;
 
-    while (true) {
+    for (;;) {
         printf("Hello, world!\n");
-        sleep_ms(1000);
+        vTaskDelay(1000);
     }
 }
+
+static void prvSetupHardware(void) { stdio_init_all(); }
+
+int main() {
+    prvSetupHardware();
+
+
+    static TaskHandle_t exampleTaskHandle;
+
+    printf("Example FreeRTOS Project STATS\n");
+
+    if (xTaskCreate(exampleTask, "Example Task", configMINIMAL_STACK_SIZE, NULL,
+                    tskIDLE_PRIORITY + 1, &exampleTaskHandle) != pdPASS) {
+        printf("Task not created\n");
+    }
+
+    /* Start the scheduler. */
+    vTaskStartScheduler();
+
+    return 0;
+}
+
+void vApplicationMallocFailedHook(void) { configASSERT((volatile void*)NULL); }
+
+
+void vApplicationIdleHook(void) {
+    volatile size_t xFreeHeapSpace;
+
+    xFreeHeapSpace = xPortGetFreeHeapSize();
+
+    (void)xFreeHeapSpace;
+}
+
+#if (configCHECK_FOR_STACK_OVERFLOW > 0)
+
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char* pcTaskName) {
+    /* Check pcTaskName for the name of the offending task,
+     * or pxCurrentTCB if pcTaskName has itself been corrupted. */
+    (void)xTask;
+    (void)pcTaskName;
+}
+
+#endif /* #if ( configCHECK_FOR_STACK_OVERFLOW > 0 ) */
